0f91a546b5e9e8aed9a50cfd0323ecd917cacec0
Hossam Othman <rowheadrex@gmail.com>
2023-08-28 12:37:39 +0200

    Check Winner

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const X_CLASS = 'x';
 const CIRCLE_CLASS = 'circle';
+const WINNING_COMBINATION = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
 const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board')
 let circleTurn;
@@ -24,7 +34,16 @@ function handleClick(e) {
     placeMark(cell, currentClass);
 
     // Check for win
-
+    if (checkWin(currentClass)) {
+        console.log("winner");
+    };
+    function checkWin(currentClass) {
+        return WINNING_COMBINATION.some(comination => {
+            return comination.every(index => {
+                return cellElements[index].classList.contains(currentClass)
+            } )
+        })
+    };
     // Check for Draw
 
     // Switch Turns
